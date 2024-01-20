fd9387e212d80e464263ae5272b5ecdf3f563792
purpleclay <purpleclaygh@gmail.com>
2024-01-20 07:23:16 +0000

    use reflection to create nix attribute set

--- scrape/main.go
+++ scrape/main.go
@@ -27,85 +27,66 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/purpleclay/chomp"
 	"github.com/spf13/cobra"
 )
 
-// TODO: write custom marshaller that understands the nix attribute
-
+// Scrape ...
 type Scrape struct {
-	Date        string `nix:"d"`
-	Version     string `nix:"v"`
-	DarwinAmd64 Target `nix:"_0,omitempty"`
-	DarwinArm64 Target `nix:"_1,omitempty"`
-	LinuxAmd32  Target ``
-	LinuxAmd64  Target ``
-	LinuxArm64  Target ``
-	LinuxArmV6  Target ``
-	// Other Linux
-	AixPPC64       Target ``
-	DragonFlyAmd64 Target ``
-	FreeBSDAmd32   Target ``
-	FreeBSDAmd64   Target ``
-	FreeBSDArm64   Target ``
-	FreeBSDArmV6   Target ``
-	FreeBSDRiscV64 Target ``
-	IllumosAmd64   Target ``
-	// NetBSD
-	// OpenBSD
-	// Plan9
-	// Solaris
-	// Windows
+	// Date ...
+	Date string `nix:"d"`
+	// Version ...
+	Version         string  `nix:"v"`
+	Darwinx86_64    *Target `nix:"_0,omitempty"`
+	DarwinARM64     *Target `nix:"_1,omitempty"`
+	Linuxx86        *Target `nix:"_2,omitempty"`
+	Linuxx86_64     *Target `nix:"_3,omitempty"`
+	LinuxARM64      *Target `nix:"_4,omitempty"`
+	LinuxARMv6      *Target `nix:"_5,omitempty"`
+	LinuxLoong64    *Target `nix:"_6,omitempty"`
+	LinuxMIPS       *Target `nix:"_7,omitempty"`
+	LinuxMIPS64     *Target `nix:"_8,omitempty"`
+	LinuxMIPS64le   *Target `nix:"_9,omitempty"`
+	LinuxMIPSle     *Target `nix:"_10,omitempty"`
+	LinuxPPC64      *Target `nix:"_11,omitempty"`
+	LinuxPPC64le    *Target `nix:"_12,omitempty"`
+	LinuxRiscv64    *Target `nix:"_13,omitempty"`
+	LinuxS390x      *Target `nix:"_14,omitempty"`
+	AixPPC64        *Target `nix:"_15,omitempty"`
+	DragonFlyx86_64 *Target `nix:"_16,omitempty"`
+	FreeBSDx86      *Target `nix:"_17,omitempty"`
+	FreeBSDx86_64   *Target `nix:"_18,omitempty"`
+	FreeBSDARM64    *Target `nix:"_19,omitempty"`
+	FreeBSDARMv6    *Target `nix:"_20,omitempty"`
+	FreeBSDRiscv64  *Target `nix:"_21,omitempty"`
+	Illumosx86_64   *Target `nix:"_22,omitempty"`
+	NetBSDx86       *Target `nix:"_23,omitempty"`
+	NetBSDx86_64    *Target `nix:"_24,omitempty"`
+	NetBSDARM64     *Target `nix:"_25,omitempty"`
+	NetBSDARMv6     *Target `nix:"_26,omitempty"`
+	OpenBSDx86      *Target `nix:"_27,omitempty"`
+	OpenBSDx86_64   *Target `nix:"_28,omitempty"`
+	OpenBSDARM64    *Target `nix:"_29,omitempty"`
+	OpenBSDARMv6    *Target `nix:"_30,omitempty"`
+	Plan9x86        *Target `nix:"_31,omitempty"`
+	Plan9x86_64     *Target `nix:"_32,omitempty"`
+	Plan9ARMv6      *Target `nix:"_33,omitempty"`
+	Solarisx86_64   *Target `nix:"_34,omitempty"`
+	Windowsx86      *Target `nix:"_35,omitempty"`
+	Windowsx86_64   *Target `nix:"_36,omitempty"`
+	WindowsARM64    *Target `nix:"_37,omitempty"`
+	WindowsARMv6    *Target `nix:"_38,omitempty"`
 }
 
-// TODO: do all new versions of Go include these os + arch combinations?
-
-/*
-- aixppc64
-- macOSx86-64
-- macOSARM64
-- dragonflyx86-64
-- FreeBSDx86
-- FreeBSDx86-64
-- FreeBSDARM64
-- FreeBSDARMv6
-- FreeBSDriscv64
-- illumosx86-64
-- Linuxx86
-- Linuxx86-64
-- LinuxARM64
-- LinuxARMv6
-Linuxloong64
-Linuxmips
-Linuxmips64
-Linuxmips64le
-Linuxmipsle
-Linuxppc64
-Linuxppc64le
-Linuxriscv64
-Linuxs390x
-netbsdx86
-netbsdx86-64
-netbsdARM64
-netbsdARMv6
-openbsdx86
-openbsdx86-64
-openbsdARM64
-openbsdARMv6
-plan9x86
-plan9x86-64
-plan9ARMv6
-solarisx86-64
-Windowsx86
-Windowsx86-64
-WindowsARM64
-WindowsARMv6
-*/
-
+// Target ...
 type Target struct {
+	// SHA ...
 	SHA string `nix:"s"`
+	// URL ...
 	URL string `nix:"u"`
 }
 
@@ -119,8 +100,14 @@ func execute(out io.Writer) error {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: optionally write output to a file or to stdout
-			return scrape(rel)
+			s, err := scrape(rel)
+			if err != nil {
+				return err
+			}
+
+			// TODO: print out to buffer (stdout) or file if one is provided
+			fmt.Println(marshal(*s))
+			return nil
 		},
 	}
 
@@ -130,12 +117,27 @@ func execute(out io.Writer) error {
 	return cmd.Execute()
 }
 
-func scrape(rel string) error {
-	// TODO: if latest, query :> https://go.dev/VERSION?m=text (returns the latest version)
+func scrape(rel string) (*Scrape, error) {
+	if rel == "latest" {
+		ver, err := get("https://go.dev/VERSION?m=text")
+		if err != nil {
+			return nil, err
+		}
+		_, rel, _ = chomp.Any("go.1234567890")(ver)
+	}
 
-	resp, err := http.Get("https://go.dev/dl/")
+	page, err := get("https://go.dev/dl/")
 	if err != nil {
-		return err
+		return nil, err
+	}
+
+	return parse(page, rel)
+}
+
+func get(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
 	}
 
 	if resp.StatusCode != http.StatusOK {
@@ -145,18 +147,14 @@ func scrape(rel string) error {
 	defer resp.Body.Close()
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	// TODO: scrape the latest version (HTTP Get and parse the output)
-	// TODO: serialise the output as a Nix attribute set
-	s := parse(string(data), rel)
-	fmt.Printf("%#v\n", s)
-	return nil
+	return string(data), nil
 }
 
-func parse(in string, rel string) Scrape {
-	s := Scrape{
+func parse(in string, rel string) (*Scrape, error) {
+	s := &Scrape{
 		Date:    time.Now().Format(time.DateOnly),
 		Version: rel,
 	}
@@ -167,7 +165,6 @@ func parse(in string, rel string) Scrape {
 	rem := in
 	for {
 		if rem, ext, err = chomp.Pair(href(rel), target())(rem); err != nil {
-			// TODO: handle error properly
 			break
 		}
 
@@ -175,24 +172,98 @@ func parse(in string, rel string) Scrape {
 			continue
 		}
 
-		t := Target{URL: ext[0], SHA: ext[5]}
+		t := &Target{SHA: ext[5], URL: ext[0]}
 
+		// TODO: need to check this against the parsed output, to ensure structure has been built correctly
 		switch ext[2] + ext[3] {
 		case "macOSx86-64":
-			s.DarwinAmd64 = t
+			s.Darwinx86_64 = t
 		case "macOSARM64":
-			s.DarwinArm64 = t
+			s.DarwinARM64 = t
+		case "Linuxx86":
+			s.Linuxx86 = t
+		case "Linuxx86-64":
+			s.Linuxx86_64 = t
+		case "LinuxARM64":
+			s.LinuxARM64 = t
+		case "LinuxARMv6":
+			s.LinuxARMv6 = t
+		case "Linuxloong64":
+			s.LinuxLoong64 = t
+		case "Linuxmips":
+			s.LinuxMIPS = t
+		case "Linuxmips64":
+			s.LinuxMIPS64 = t
+		case "Linuxmips64le":
+			s.LinuxMIPS64le = t
+		case "Linuxmipsle":
+			s.LinuxMIPSle = t
+		case "Linuxppc64":
+			s.LinuxPPC64 = t
+		case "Linuxppc64le":
+			s.LinuxPPC64le = t
+		case "Linuxriscv64":
+			s.LinuxRiscv64 = t
+		case "Linuxs390x":
+			s.LinuxS390x = t
+		case "aixppc64":
+			s.AixPPC64 = t
+		case "dragonflyx86-64":
+			s.DragonFlyx86_64 = t
+		case "FreeBSDx86":
+			s.FreeBSDx86 = t
+		case "FreeBSDx86-64":
+			s.FreeBSDx86_64 = t
+		case "FreeBSDARM64":
+			s.FreeBSDARM64 = t
+		case "FreeBSDARMv6":
+			s.FreeBSDARMv6 = t
+		case "FreeBSDriscv64":
+			s.FreeBSDRiscv64 = t
+		case "illumosx86-64":
+			s.Illumosx86_64 = t
+		case "netbsdx86":
+			s.NetBSDx86 = t
+		case "netbsdx86-64":
+			s.NetBSDx86_64 = t
+		case "netbsdARM64":
+			s.NetBSDARM64 = t
+		case "netbsdARMv6":
+			s.NetBSDARMv6 = t
+		case "openbsdx86":
+			s.OpenBSDx86 = t
+		case "openbsdx86-64":
+			s.OpenBSDx86_64 = t
+		case "openbsdARM64":
+			s.OpenBSDARM64 = t
+		case "openbsdARMv6":
+			s.OpenBSDARMv6 = t
+		case "plan9x86":
+			s.Plan9x86 = t
+		case "plan9x86-64":
+			s.Plan9x86_64 = t
+		case "plan9ARMv6":
+			s.Plan9ARMv6 = t
+		case "solarisx86-64":
+			s.Solarisx86_64 = t
+		case "Windowsx86":
+			s.Windowsx86 = t
+		case "Windowsx86-64":
+			s.Windowsx86_64 = t
+		case "WindowsARM64":
+			s.WindowsARM64 = t
+		case "WindowsARMv6":
+			s.WindowsARMv6 = t
 		}
 	}
-	return s
+	return s, nil
 }
 
-// TODO: combine href and target together
 func href(ver string) chomp.Combinator[string] {
 	return func(s string) (string, string, error) {
 		rem, ext, err := chomp.All(
-			chomp.Until(`href="/dl/`+ver),
-			chomp.Delimited(chomp.Tag(`href="`), chomp.Until(`"`), chomp.Tag(`"`)),
+			chomp.Until(`<a class="download" href="/dl/`+ver),
+			chomp.Delimited(chomp.Tag(`<a class="download" href="`), chomp.Until(`"`), chomp.Tag(`"`)),
 			eol())(s)
 		if err != nil {
 			return rem, "", err
@@ -238,6 +309,63 @@ func tableCell(deliml, delimr string) chomp.Combinator[string] {
 
 		return rem, ext[1], nil
 	}
+}
+
+func marshal(s Scrape) (string, error) {
+	var buf strings.Builder
+	buf.WriteString("{")
+
+	values := reflect.ValueOf(s)
+	types := values.Type()
+	for i := 0; i < values.NumField(); i++ {
+		v := values.Field(i)
+		f := types.Field(i)
+		tags := strings.Split(f.Tag.Get("nix"), ",")
+
+		var omitempty bool
+		for _, tag := range tags[1:] {
+			if tag == "omitempty" {
+				omitempty = true
+			}
+		}
+
+		switch f.Type.Kind() {
+		case reflect.String:
+			buf.WriteString(fmt.Sprintf(`%s="%s";`, tags[0], v.String()))
+		case reflect.Ptr:
+			if f.Type.Elem().Name() == "Target" {
+				isNil := v.Pointer() == 0
+				if isNil && omitempty {
+					continue
+				}
+
+				buf.WriteString(tags[0])
+				buf.WriteString("={")
+
+				if !isNil {
+					target := reflect.Indirect(v)
+					ttypes := target.Type()
+
+					for j := 0; j < target.NumField(); j++ {
+						tv := target.Field(j)
+						tf := ttypes.Field(j)
+						ttags := strings.Split(tf.Tag.Get("nix"), ",")
+
+						switch tf.Type.Kind() {
+						case reflect.String:
+							buf.WriteString(fmt.Sprintf(`%s="%s";`, ttags[0], tv.String()))
+						}
+					}
+				}
+
+				buf.WriteString("};")
+			}
+		}
+	}
+
+	buf.WriteString("}")
+
+	return buf.String(), nil
 }
 
 func main() {
