c0a2fcf2b411ce6ed0ce387baaf8778b9b98cb5f
Purple Clay <purpleclaygh@gmail.com>
2025-12-06 13:56:26 +0000

    chore: manage dependencies with renovate using custom presets (#12)

--- flake.nix
+++ flake.nix
@@ -2,20 +2,27 @@
   description = "Pure and reproducible nix overlay of binary distributed golang toolchains";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [ go ];
-        };
-      }
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              alejandra
+              go
+              nil
+            ];
+          };
+        }
     );
 }
