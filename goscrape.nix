ab7e36f9faaa23625de544b19a7a336b721a51bf
Purple Clay <purpleclaygh@gmail.com>
2025-12-19 05:48:27 +0000

    ci: ensure consistent naming of tools throughout workflows and codebase (#67)
    Closes #66

--- goscrape.nix
+++ goscrape.nix
@@ -3,11 +3,11 @@
   go,
 }:
 (pkgs.buildGoModule.override {inherit go;}) {
-  pname = "go-scrape";
+  pname = "goscrape";
   version = "dev";
   src = ./.;
   subPackages = ["cmd/goscrape"];
   env.CGO_ENABLED = 0;
   doCheck = false;
-  vendorHash = "sha256-ig8Hs5uZ4CEplU/6YNDNNfBKy9FKa/Zcvt5dzpiVwhM=";
+  vendorHash = "sha256-bLfVDTHC47JhIJgAVmpFSWSfyIJCltpy8ntfDCs19/w=";
 }
--- govendor.nix
+++ govendor.nix
@@ -3,11 +3,11 @@
   go,
 }:
 (pkgs.buildGoModule.override {inherit go;}) {
-  pname = "go-vendor";
+  pname = "govendor";
   version = "dev";
   src = ./.;
   subPackages = ["cmd/govendor"];
   env.CGO_ENABLED = 0;
   doCheck = false;
-  vendorHash = "sha256-ig8Hs5uZ4CEplU/6YNDNNfBKy9FKa/Zcvt5dzpiVwhM=";
+  vendorHash = "sha256-bLfVDTHC47JhIJgAVmpFSWSfyIJCltpy8ntfDCs19/w=";
 }
