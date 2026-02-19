5c80e2a70445d09b3d34a476518ebf4d6ff617dc
purpleclay <purpleclaygh@gmail.com>
2026-02-19 05:26:44 +0000

    ci: move integration tests to flake checks and adopt nix-fast-build
    Closes #235
    
    Integration tests are now registered as flake checks via a new
    test/integration/default.nix, mirroring the existing lib test pattern.
    Adding a new integration test only requires wiring it into this file
    rather than updating the CI workflow.
    
    The tests job is replaced with a checks job that uses nix-fast-build to
    evaluate and build all checks in parallel. The in-tree vendor tests
    remain as manual CI steps since they require vendor directory generation
    before building.
    
    Signed-off-by: purpleclay <purpleclaygh@gmail.com>

---
+++ test/integration/default.nix
@@ -0,0 +1,43 @@
+{pkgs}: let
+  go_1_22 = pkgs.go-bin.versions."1.22.3";
+  go_1_25 = pkgs.go-bin.versions."1.25.0";
+in {
+  integration-build-go-module = import ./build-go-module {
+    inherit pkgs;
+    go = go_1_22;
+  };
+  integration-local-replace = import ./local-replace {
+    inherit pkgs;
+    go = go_1_22;
+  };
+  integration-local-replace-external = import ./local-replace-external {
+    inherit pkgs;
+    go = go_1_22;
+  };
+  integration-indirect-deps = import ./indirect-deps {
+    inherit pkgs;
+    go = go_1_22;
+  };
+  integration-workspace-api =
+    (import ./workspace {
+      inherit pkgs;
+      go = go_1_22;
+    }).api;
+  integration-workspace-worker =
+    (import ./workspace {
+      inherit pkgs;
+      go = go_1_22;
+    }).worker;
+  integration-workspace-no-gowork = import ./workspace-no-gowork {
+    inherit pkgs;
+    go = go_1_22;
+  };
+  integration-check-phase = import ./check-phase {
+    inherit pkgs;
+    go = go_1_22;
+  };
+  integration-tool-directive-codegen = import ./tool-directive-codegen {
+    inherit pkgs;
+    go = go_1_25;
+  };
+}
