5ecabb69910fe4558ee35049a72267a61e7f5576
purpleclay <purpleclaygh@gmail.com>
2026-04-13 20:58:09 +0100

    feat(nix): improve default template with default.nix and drift detection
    Closes #363
    
    Extract the buildGoApplication derivation into a separate default.nix,
    matching the convention used across all go-overlay examples. Add
    cachix/git-hooks.nix with a govendor --check pre-commit hook that runs
    on go.mod/go.work changes, wired into the dev shell via shellHook so nix
    develop installs it automatically.
    
    Signed-off-by: purpleclay <purpleclaygh@gmail.com>

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -5,9 +5,20 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    go-overlay.url = "github:purpleclay/go-overlay";
-    go-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    go-overlay.inputs.flake-utils.follows = "flake-utils";
+    go-overlay = {
+      url = "github:purpleclay/go-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs = {
@@ -15,6 +26,7 @@
     nixpkgs,
     flake-utils,
     go-overlay,
+    git-hooks,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -24,21 +36,30 @@
           overlays = [go-overlay.overlays.default];
         };
         go = pkgs.go-bin.fromGoMod ./go.mod;
-      in {
-        packages.default = pkgs.buildGoApplication {
-          pname = "example";
-          version = "0.1.0";
-          src = ./.;
-          inherit go;
-          modules = ./govendor.toml;
-        };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go.withDefaultTools
-            go-overlay.packages.${system}.govendor
-          ];
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks.govendor = {
+            enable = true;
+            name = "govendor";
+            entry = "${go-overlay.packages.${system}.govendor}/bin/govendor --check";
+            files = "(^|/)go\\.(mod|work)$";
+            pass_filenames = true;
+          };
         };
-      }
+      in
+        with pkgs; {
+          packages.default = callPackage ./default.nix {inherit go;};
+
+          devShells.default = mkShell {
+            inherit (pre-commit-check) shellHook;
+            buildInputs =
+              [
+                go.withDefaultTools
+                go-overlay.packages.${system}.govendor
+              ]
+              ++ pre-commit-check.enabledPackages;
+          };
+        }
     );
 }
