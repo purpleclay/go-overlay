5cac99a310bd40c3f61d96be74fba63751fb7fad
purpleclay <purpleclaygh@gmail.com>
2026-05-01 08:27:11 +0100

    refactor(govendor): replace mvdan.cc/sh with os/exec in exec.go
    Closes #423
    
    Signed-off-by: purpleclay <purpleclaygh@gmail.com>

--- go.mod
+++ go.mod
@@ -16,7 +16,6 @@ require (
 	github.com/stretchr/testify v1.11.1
 	golang.org/x/mod v0.35.0
 	gotest.tools/v3 v3.5.2
-	mvdan.cc/sh/v3 v3.13.1
 )
 
 require (
@@ -36,6 +35,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.19 // indirect
@@ -47,12 +47,12 @@ require (
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
-	golang.org/x/term v0.41.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/mod/exec.go
+++ internal/mod/exec.go
@@ -2,14 +2,9 @@ package mod
 
 import (
 	"bytes"
-	"context"
-	"io"
 	"os"
+	"os/exec"
 	"strings"
-
-	"mvdan.cc/sh/v3/expand"
-	"mvdan.cc/sh/v3/interp"
-	"mvdan.cc/sh/v3/syntax"
 )
 
 type execError struct {
@@ -28,56 +23,19 @@ func (e *execError) Unwrap() error {
 	return e.err
 }
 
-type devNull struct{}
-
-func (devNull) Read(_ []byte) (int, error) {
-	return 0, io.EOF
-}
-
-func (devNull) Write(p []byte) (int, error) {
-	return len(p), nil
-}
-
-func (devNull) Close() error {
-	return nil
-}
-
-func exec(args []string, dir string) (string, error) {
+func execCmd(args []string, dir string) (string, error) {
 	return execWithEnv(args, dir, nil)
 }
 
 func execWithEnv(args []string, dir string, extraEnv []string) (string, error) {
-	cmd := strings.Join(args, " ")
-	p, err := syntax.NewParser().Parse(strings.NewReader(cmd), "")
-	if err != nil {
-		return "", err
-	}
-
-	env := append(os.Environ(), extraEnv...)
-
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Dir = dir
+	cmd.Env = append(os.Environ(), extraEnv...)
 	var stdout, stderr bytes.Buffer
-	r, err := interp.New(
-		interp.Params("-e"),
-		interp.StdIO(os.Stdin, &stdout, &stderr),
-		interp.OpenHandler(openHandler),
-		interp.Env(expand.ListEnviron(env...)),
-		interp.Dir(dir),
-	)
-	if err != nil {
-		return "", err
-	}
-
-	if err := r.Run(context.Background(), p); err != nil {
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
 		return "", &execError{err: err, stderr: stderr.String()}
 	}
-
 	return stdout.String(), nil
-}
-
-func openHandler(ctx context.Context, path string, flag int, perm os.FileMode) (io.ReadWriteCloser, error) {
-	if path == "/dev/null" {
-		return devNull{}, nil
-	}
-
-	return interp.DefaultOpenHandler()(ctx, path, flag, perm)
 }
